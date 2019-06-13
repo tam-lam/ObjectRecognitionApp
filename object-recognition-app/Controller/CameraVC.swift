825cea34c3ff05de85bf2a069fb4879126a8a676
Tam Lam <imtamtlam@gmail.com>
2019-06-13 18:30:39 +1000

    capture photo, show preview

--- object-recognition-app/Controller/CameraVC.swift
+++ object-recognition-app/Controller/CameraVC.swift
@@ -12,7 +12,7 @@ import AVFoundation
 class CameraVC: UIViewController {
     
     var captureSession: AVCaptureSession!
-    var cameraOutput: AVCaptureOutput!
+    var cameraOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
     
     @IBOutlet weak var roundedLblView: RoundedShadowView!
@@ -21,6 +21,9 @@ class CameraVC: UIViewController {
     @IBOutlet weak var identificationLbl: UILabel!
     @IBOutlet weak var captureImageView: UIImageView!
     @IBOutlet weak var flashBtn: RoundedShadowButton!
+    
+    var photoData : Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -33,6 +36,10 @@ class CameraVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTapsRequired = 1
+        
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
         
@@ -53,14 +60,34 @@ class CameraVC: UIViewController {
                 previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
                 
                 cameraView.layer.addSublayer(previewLayer)
+                cameraView.addGestureRecognizer(tap)
                 captureSession.startRunning()
             }
-            
             
         }catch {
             debugPrint(error)
         }
     }
     
+    @objc func didTapCameraView() {
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType, kCVPixelBufferWidthKey as String:60, kCVPixelBufferHeightKey as String:160]
+        settings.previewPhotoFormat = previewFormat
+        
+        cameraOutput.capturePhoto(with: settings, delegate: self)
+    }
 }
-
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error{
+            debugPrint(error)
+        } else{
+            photoData = photo.fileDataRepresentation()
+            
+            let image = UIImage(data: photoData!)
+            self.captureImageView.image = image
+        }
+        
+    }
+}
